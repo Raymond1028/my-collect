116efe00383dc4ffc256717c9e840d59d226dff8
Ray's Git <lancelot53.ra@gmail.com>
2021-12-10 12:05:08 -0500

    Completed Assignment

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+ def my_collect(collection)
+    my_collection = []
+    i = 0
 
+    while collection.length >  i
+       my_collection << yield(collection[i])
+       i+=1
+    end
+    
+    my_collection
+end
 
